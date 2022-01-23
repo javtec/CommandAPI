4a8f38b6057d8ab0520cee9f1a19b127157106cb
Juan Vega <juan.vega2@gmail.com>
2022-01-23 01:13:52 -0600

    Added Model and Mock Repository

--- src/CommandAPI/Data/MockCommandAPIRepo.cs
+++ src/CommandAPI/Data/MockCommandAPIRepo.cs
@@ -17,12 +17,33 @@ namespace CommandAPI.Data
 
         public IEnumerable<Command> GetAllCommands()
         {
-            throw new System.NotImplementedException();
+            var commands = new List<Command>
+            {
+                new Command{
+                Id=0, HowTo="How to generate a migration",
+                CommandLine="dotnet ef migrations add <Name of Migration>",
+                Platform=".Net Core EF"},
+                new Command{
+                Id=1, HowTo="Run Migrations",
+                CommandLine="dotnet ef database update",
+                Platform=".Net Core EF"},
+                new Command{
+                Id=2, HowTo="List active migrations",
+                CommandLine="dotnet ef migrations list",
+                Platform=".Net Core EF"}
+            };
+            return commands;
         }
 
         public Command GetCommandById(int id)
         {
-            throw new System.NotImplementedException();
+            return new Command
+            {
+                Id = 0,
+                HowTo = "How to generate a migration",
+                CommandLine = "dotnet ef migrations add <Name of Migration>",
+                Platform = ".Net Core EF"
+            };
         }
 
         public bool SaveChanges()
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -1,15 +1,17 @@
+using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 namespace CommandAPI
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            //SECTION 1. Add code below
             services.AddControllers();
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
