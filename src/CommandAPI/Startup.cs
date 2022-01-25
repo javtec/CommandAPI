6d5c677bced875db45fb42a21007e6925e53c063
Juan Vega <juan.vega2@gmail.com>
2022-01-25 00:29:33 -0600

    JAV 01/25/2021 12:28AM CST: Added appsettings.Development.json and removed userid and password

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
@@ -19,8 +20,14 @@ namespace CommandAPI
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
-            (Configuration.GetConnectionString("PostgreSqlConnection")));
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<CommandContext>
+                (opt => opt.UseNpgsql(builder.ConnectionString));
+
             services.AddControllers();
             // services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
